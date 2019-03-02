239499ff79e6fbde69b4712173c981107741f797
James K Nelson <james@jamesknelson.com>
2019-03-02 20:29:20 +0900

    update redux-custom-hooks title

--- daishi-kato/articles/2019/02-25-redux-custom-hooks/article.tsx
+++ daishi-kato/articles/2019/02-25-redux-custom-hooks/article.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { createDocumentPage } from 'shared/pages/createDocumentPage'
 
 export default createDocumentPage({
-  title: "Integrating React and Redux, with Hooks and Proxies",
+  title: "Creating a react-redux alternative with Hooks and Proxies",
   tags: ['react', 'redux'],
   blurb: (
     <p>
